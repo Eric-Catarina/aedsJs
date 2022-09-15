083702d6c930549603576eb9e3f79aee945d8d96
Eric-Catarina <2020954200@teiacoltec.org>
2022-09-15 10:23:29 -0300

    feat: ToString Method LL

--- data_structures/linked_list.js
+++ data_structures/linked_list.js
@@ -9,29 +9,34 @@ class LinkedList {
     }
 
     push = (element) =>{
-        const node = new Node
+        const node = new Node(element)
         let currentNode
         if (this.head == undefined){
 
-            this.head = element
+            this.head = node
         }
         else{
             currentNode = this.head
-            while(currentNode.next != null){
+            while(currentNode.next != null){ // Travels along all the list until the last node
+
                 currentNode = currentNode.next
                 console.log(currentNode)
 
             }
-            currentNode.next = node
+            currentNode.next = node // When arriving the last node, assign the final node.next as the new Node
         }
 
         this.count ++
     }
+
     toString = () => {
         let currentNode = this.head
+        console.log(currentNode.element)
 
         while(currentNode.next != null){
             currentNode = currentNode.next
+            console.log(currentNode.element)
+
         }
     }
 }
@@ -47,10 +52,13 @@ class Node {
 
 myList = new LinkedList
 myListTwo = new LinkedList
+
+
 myList.push("a")
 myList.push("b")
-
 myList.push("c")
+myList.push("d")
+myList.push("e")
 
-myListTwo.push("a")
 
+myList.toString()
