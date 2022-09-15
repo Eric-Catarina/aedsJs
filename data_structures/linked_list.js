7318bca626eebde0856ca448f125621c7b103620
Eric-Catarina <2020954200@teiacoltec.org>
2022-09-14 22:20:11 -0300

    feat> push Method LinkedList

--- data_structures/linked_list.js
+++ data_structures/linked_list.js
@@ -3,4 +3,44 @@ class LinkedList {
         this.count = 0
         this.head = undefined
     }
+
+    equalsFn = (a,b) => {
+        return a === b
+    }
+
+    push = (element) =>{
+        const node = new Node
+        let currentNode
+        if (this.head == undefined){
+
+            this.head = element
+        }
+        else{
+            currentNode = this.head
+            while(currentNode.next != null){
+                currentNode = currentNode.next
+            }
+            currentNode = currentNode.next
+        }
+
+
+        this.count ++
+
+    }
+
 }
+
+
+class Node {
+    constructor (element){
+        this.element = element;
+        this.next = undefined;
+    }
+
+}
+
+myList = new LinkedList
+myList.push("a")
+myList.push("b")
+
+console.log(myList.head)
