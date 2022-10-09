d2545d1fa98bee5349407aa42f4efff9dc1b2d95
Eric-Catarina <2020954200@teiacoltec.org>
2022-10-08 21:15:48 -0300

    feat: toString() (Graphs)

--- data_structures/graph.js
+++ data_structures/graph.js
@@ -2,13 +2,32 @@
 
 
 class Graph {
-    constructor(isDirected = false){
+    constructor(isDirected = false) {
         this.isDirected = isDirected
         this.vertices = []
         this.adjList = new Map
     }
-    addVertex = (vertex) =>{
-        if(!this.vertices.includes(vertex)){
+
+    toString = () => {
+        let s = ''
+        for (let i = 0; i < this.vertices.length; i++) {
+            s += `${this.vertices[i]} --> ` 
+            let neighbors = this.adjList.get(this.vertices[i])
+            if (neighbors == undefined){
+                neighbors = ""
+            }
+
+            for (const value of neighbors){
+                s += `${value} `
+            }
+            s += '\n'
+
+        }
+        return s
+    }
+
+    addVertex = (vertex) => {
+        if (!this.vertices.includes(vertex)) {
             this.vertices.push(vertex)
         }
     }
@@ -16,33 +35,33 @@ class Graph {
         this.addVertex(origin)
         this.addVertex(destiny)
 
-        if (this.adjList.get(origin)){
-            if (this.adjList.get(origin).includes(destiny)){   // If the adjList already have the value
+        if (this.adjList.get(origin)) {
+            if (this.adjList.get(origin).includes(destiny)) {   // If the adjList already have the value
                 return false
             }
             this.adjList.get(origin).push(destiny)
             return true
         }
-            this.adjList.set(origin, [])
-            this.adjList.get(origin).push(destiny)
-            return true
+        this.adjList.set(origin, [])
+        this.adjList.get(origin).push(destiny)
+        return true
     }
 }
 
 myGraph = new Graph()
 const myVertices = ['A', 'B', 'C', 'D']
-for (element of myVertices){
+for (element of myVertices) {
     myGraph.addVertex(element)
 }
 
 myGraph.addEdge('A', 'B')
 myGraph.addEdge('A', 'C')
 myGraph.addEdge('A', 'D')
-myGraph.addEdge('A', 'D')
 
-myGraph.addEdge('C', 'D')
-myGraph.addEdge('B', 'D')
+myGraph.addEdge('B', 'A')
 myGraph.addEdge('B', 'C')
 
+myGraph.addEdge('C', 'D')
 
-console.log(myGraph.adjList)
+
+console.log(myGraph.toString())
